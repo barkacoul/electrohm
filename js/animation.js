dde53627b957cea9f847012013634feee9dba478
Pape Barka <53663919+barkacoul@users.noreply.github.com>
2020-01-22 18:18:33 +0000

    Add files via upload
    responsive & animation

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({duration: 3000});
+sr.reveal('.contenu-site .d-flex');
+sr.reveal('.section-2 .card');
+sr.reveal('.section-2 .d-flex');
+sr.reveal('.section-2 .col-md-4');
+sr.reveal('.section-2 .col-sm-12');
+sr.reveal('.section-3 .col-md-7, .section-3 .col-md-5');
+sr.reveal('.section-3 .col-sm-12, .section-3 .col-sm-12');
